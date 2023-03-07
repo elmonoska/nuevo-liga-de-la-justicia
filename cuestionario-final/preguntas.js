f6117ed2a5ee26fe8989a3666de8d1fb8b4420df
Raul CF <elmonoska@outlook.com>
2023-03-07 13:00:54 -0600

    Agregando preguntas al cuestionario

--- cuestionario-final/preguntas.js
+++ cuestionario-final/preguntas.js
@@ -1,9 +1,29 @@
 /**
  * -------------------------------------------------------------------
- * Este archivo edita las preguntas
+ * Este archivo contiene todas las preguntas del examen
  * -------------------------------------------------------------------
  *  */ 
+
+//temas de cada pregunta
+const tema1 = 'Verb to be - Articles. Read and complete with the correct option:'
+const tema2 = 'Verb to be - Negative and interrogative forms. Read and complete with the correct option:'
+const tema3 = 'Question words. Read and complete with the correct option:'
+const tema4 = 'Possessive pronouns. Read and complete with the correct option:'
+const tema5 = 'Possessive’s. Read and complete with the correct option:'
+const tema6 = 'Demonstrative pronouns. Read and complete with the correct option:'
+const tema7 = 'Prepositions. Look at the pictures and answer the questions:'
+const tema8 = 'Adjectives and adverbs. Read and complete correctly. (ex: loud = loudly):'
+const tema9 = 'Intensifiers. Complete with the correct answer:'
+const tema10 = 'Simple present. Complete with the correct answer:'
+const tema11 = 'Simple present. Select affirmative, negative or interrogative sentences:'
+const tema12 = 'Present tense. Complete with the correct answer:'
+
+
+
+
+//preguntas
 export const examenFinal = [
+    //Preguntas del tema 1
     {
         pregunta: 'Pregunta de ejemplo, cualquier opcion es correcta.',
         opciones: [
@@ -16,7 +36,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option:  I  _ _  English student.',
+        pregunta: `${tema1} I _ _ English student.`,
         opciones: [
             'am, a',
             'am, an',
@@ -27,7 +47,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: She _ _ Super Hero fan.',
+        pregunta: `${tema1} She _ _ Super Hero fan.`,
         opciones: [
             'am, a',
             'am, an',
@@ -38,7 +58,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: _ you happy?',
+        pregunta: `${tema1} _ you happy?`,
         opciones: [
             'Are',
             'Am',
@@ -48,7 +68,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: Green Lantern _ not a villain',
+        pregunta: `${tema1} Green Lantern _ not a villain.`,
         opciones: [
             'am',
             'are',
@@ -58,7 +78,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: My father and I _ football fans"',
+        pregunta: `${tema1} My father and I _ football fans."`,
         opciones: [
             'am',
             'are',
@@ -68,7 +88,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: _ Wonder Woman _ Amazon princess?',
+        pregunta: `${tema1} _ Wonder Woman _ Amazon princess?`,
         opciones: [
             'is, a',
             'is, an',
@@ -79,7 +99,7 @@ export const examenFinal = [
         img: '',
     },
     {
-        pregunta: 'Verb to be - Articles. Read and complete with the correct option: Today _ _ fantastic day!',
+        pregunta: `${tema1} Today _ _ fantastic day!`,
         opciones: [
             'is, a',
             'is, an',
@@ -87,6 +107,635 @@ export const examenFinal = [
             'are, an',
         ],
         respuesta: 'is, a',
+        img: '',
+    },
+
+    //Preguntas del tema 2
+    {
+        pregunta: `${tema2} Wonder Woman _ a doctor, she _ a Super Hero.`,
+        opciones: [
+            'is not, are',
+            'is not, is',
+            'are not, are',
+            'are not, is',
+        ],
+        respuesta: 'is not, is',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} _ you _ artist or a musician?`,
+        opciones: [
+            'Am, a',
+            'Am, an',
+            'Are, a',
+            'Are, an',
+        ],
+        respuesta: 'Are, an',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} They _ friends, but they are nice.`,
+        opciones: [
+            'isn\'t',
+            'aren\'t',
+            'not',
+        ],
+        respuesta: 'aren\'t',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} It _ hot today, I am so happy!`,
+        opciones: [
+            'isn\'t',
+            'aren\'t',
+            'not',
+        ],
+        respuesta: 'isn\'t',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} I _ a zombie.`,
+        opciones: [
+            'am not',
+            'are not',
+            'is not',
+        ],
+        respuesta: 'am not',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} _ they wizards? No, they _.`,
+        opciones: [
+            'Am, isn\'t',
+            'Am, aren\'t',
+            'Are, isn\'t',
+            'Are aren\'t',
+            'Is, isn\'t',
+            'Is, aren\'t',
+        ],
+        respuesta: 'Are, aren\'t',
+        img: '',
+    },
+    {
+        pregunta: `${tema2} _ the Joker a Super Hero? No, he _.`,
+        opciones: [
+            'Are, isn\'t',
+            'Are aren\'t',
+            'Is, isn\'t',
+            'Is, aren\'t',
+        ],
+        respuesta: 'Is, isn\'t',
+        img: '',
+    },
+
+    //Preguntas del tema 3
+    {
+        pregunta: `${tema3} _ is Canada?`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'Where',
+        img: '',
+    },
+    {
+        pregunta: `${tema3} _ is the interview.`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'When',
+        img: '',
+    },
+    {
+        pregunta: `${tema3} _ is the new Math teacher?`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'Who',
+        img: '',
+    },
+    {
+        pregunta: `${tema3} _ time is it?`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'What',
+        img: '',
+    },
+    {
+        pregunta: `${tema3} _ is she from? She is from Italy.`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'Where',
+        img: '',
+    },
+    {
+        pregunta: `${tema3} _ old are you? I am 12 years old.`,
+        opciones: [
+            'Who',
+            'Where',
+            'When',
+            'Why',
+            'What',
+            'How',
+        ],
+        respuesta: 'How',
+        img: '',
+    },
+
+    //Preguntas del tema 4
+    {
+        pregunta: `${tema4} Batman is friend of Superman. Is _ friend.`,
+        opciones: [
+            'her',
+            'his',
+            'its',
+        ],
+        respuesta: 'his',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} _ name is Wonder Woman.`,
+        opciones: [
+            'His',
+            'Its',
+            'Her',
+        ],
+        respuesta: 'Her',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} The Flash and you are friends. He is _ friend.`,
+        opciones: [
+            'its',
+            'your',
+            'his',
+        ],
+        respuesta: 'your',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} This little house is for my dog. Is _ house.`,
+        opciones: [
+            'its',
+            'his',
+            'her',
+        ],
+        respuesta: 'its',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} We are humans, The Earth is _ planet.`,
+        opciones: [
+            'its',
+            'our',
+            'his',
+        ],
+        respuesta: 'our',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} My name is Lucy and she is _ mom. My mom's name is Anne.`,
+        opciones: [
+            'their',
+            'my',
+            'its',
+        ],
+        respuesta: 'my',
+        img: '',
+    },
+    {
+        pregunta: `${tema4} They are Super Heroes, _ identity is a mistery.`,
+        opciones: [
+            'my',
+            'our',
+            'their',
+        ],
+        respuesta: 'their',
+        img: '',
+    },
+
+    /* //Preguntas del tema 5
+    {
+        pregunta: `${tema5} My Super Hero _ suit.`,
+        opciones: [
+            's\'',
+            '\'s',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema5} Their enemie _ powers.`,
+        opciones: [
+            's\'',
+            '\'s',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema5} Your mother _ house.`,
+        opciones: [
+            's\'',
+            '\'s',
+        ],
+        respuesta: '',
+        img: '',
+    }, */
+
+    //Preguntas del tema 6
+    {
+        pregunta: `${tema6} _ car is red and comfortable.`,
+        opciones: [
+            'This',
+            'These',
+        ],
+        respuesta: 'This',
+        img: '',
+    },
+    {
+        pregunta: `${tema6} _ flowers are beautiful.`,
+        opciones: [
+            'That',
+            'Those',
+        ],
+        respuesta: 'That',
+        img: '',
+    },
+    {
+        pregunta: `${tema6} Are _ books for me?`,
+        opciones: [
+            'this',
+            'these',
+        ],
+        respuesta: 'these',
+        img: '',
+    },
+    {
+        pregunta: `${tema6} Is _ you new house?`,
+        opciones: [
+            'that',
+            'those',
+        ],
+        respuesta: 'that',
+        img: '',
+    },
+
+    //Preguntas del tema 7
+    {
+        pregunta: `${tema7} Where is the apple?`,
+        opciones: [
+            'under',
+            'on',
+            'next to',
+        ],
+        respuesta: 'on',
+        img: '',
+    },
+    {
+        pregunta: `${tema7} Where is the cat?`,
+        opciones: [
+            'between',
+            'under',
+            'in front of',
+        ],
+        respuesta: 'between',
+        img: '',
+    },
+    {
+        pregunta: `${tema7} Where are my shoes?`,
+        opciones: [
+            'on',
+            'next to',
+            'under',
+        ],
+        respuesta: 'under',
+        img: '',
+    },
+
+/*     //Preguntas del tema 8
+    {
+        pregunta: `${tema8} The tiger is _. It attacks _. (aggresive)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema8} The Flash is a _ Super Hero. He runs _. (fast)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema8} The students are _. They study the lesson _. (quiet)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    }, */
+
+    /* //Preguntas del tema 9
+    {
+        pregunta: `${tema9} Those horses are _ fast! (too/enough)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema9} That's _ information, thank you. (enough/so)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema9} My mom is a _ good singer. (very/enough)`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    }, */
+
+    //Preguntas del tema 10
+    {
+        pregunta: `${tema10} We _ (like/likes) comics, they are fun!`,
+        opciones: [
+            'like',
+            'likes',
+        ],
+        respuesta: 'like',
+        img: '',
+    },
+    {
+        pregunta: `${tema10} Super Heroes _ (protect/protects) the planet.`,
+        opciones: [
+            'protect',
+            'protects',
+        ],
+        respuesta: 'protect',
+        img: '',
+    },
+    {
+        pregunta: `${tema10} Wonder Woman _ (help/helps) humans against evildoers.`,
+        opciones: [
+            'help',
+            'helps',
+        ],
+        respuesta: 'helps',
+        img: '',
+    },
+    {
+        pregunta: `${tema10} She _ (flight/flights) with her lasso of Truth.`,
+        opciones: [
+            'flight',
+            'flights',
+        ],
+        respuesta: 'flights',
+        img: '',
+    },
+    {
+        pregunta: `${tema10} They _ (need/needs) super powers to save humanity from Solomon Grundy and Gorilla Grodd.`,
+        opciones: [
+            'need',
+            'needs',
+        ],
+        respuesta: 'needs',
+        img: '',
+    },
+
+    //Preguntas del tema 11
+    {
+        pregunta: `${tema11} SELECT NEGATIVE My friends read comic books.`,
+        opciones: [
+            'My friends don\'t read comic books.',
+            'My friends desn\'t read comic books.',
+        ],
+        respuesta: 'My friends don\'t read comic books.',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT INTERROGATIVE My friends read comic books.`,
+        opciones: [
+            'Do my friends read comic books?',
+            'Does my friends read comic books?',
+        ],
+        respuesta: 'Does my friends read comic books?',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT NEGATIVE She watches TV on weekends.`,
+        opciones: [
+            'She doesn\'t watches TV on weekends',
+            'She doesn\'t watch TV on weekends',
+            'She don\'t watches TV on weekends',
+            'She don\'t watch TV on weekends',
+        ],
+        respuesta: 'She doesn\'t watch TV on weekends',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT INTERROGATIVE She watches TV on weekends.`,
+        opciones: [
+            'Does she watches TV on weekends?',
+            'Does she watch TV on weekends?',
+            'Do she watches TV on weekends?',
+            'Do she watch TV on weekends?',
+        ],
+        respuesta: 'Does she watch TV on weekends?',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT AFFIRMATIVE Does my sister like French fries?`,
+        opciones: [
+            'My sister like French fries.',
+            'My sister likes French fries.',
+        ],
+        respuesta: 'My sister likes French fries.',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT NEGATIVE Does my sister like French fries?`,
+        opciones: [
+            'My sister don\'t like French fries.',
+            'My sister don\'t likes French fries.',
+            'My sister doesn\'t like French fries.',
+            'My sister doesn\'t likes French fries.',
+        ],
+        respuesta: 'My sister doesn\'t like French fries.',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT AFFIRMATIVE The boy doesn't jump the rope.`,
+        opciones: [
+            'The boy jump the rope.',
+            'The boy jumps the rope.',
+        ],
+        respuesta: 'The boy jumps the rope.',
+        img: '',
+    },
+    {
+        pregunta: `${tema11} SELECT INTERROGATIVE The boy doesn't jump the rope.`,
+        opciones: [
+            'Does the boy jump the rope?',
+            'Does the boy jumps the rope?',
+            'Do the boy jump the rope?',
+            'Do the boy jumps the rope?',
+        ],
+        respuesta: 'Does the boy jump the rope?',
+        img: '',
+    },
+
+    //Preguntas del tema 12
+    {
+        pregunta: `${tema12} Pauline and Bruce _ (work/works) at a bank.`,
+        opciones: [
+            'work',
+            'works',
+        ],
+        respuesta: 'work',
+        img: '',
+    },
+    {
+        pregunta: `${tema12} They _ (is/are) siblings.`,
+        opciones: [
+            'is',
+            'are',
+        ],
+        respuesta: 'are',
+        img: '',
+    },
+    {
+        pregunta: `${tema12} She _ (are/is) the manager and he _ (is/are) a clrek.`,
+        opciones: [
+            'are, is',
+            'are, are',
+            'is, are',
+            'is, is',
+        ],
+        respuesta: 'is, is',
+        img: '',
+    },
+    {
+        pregunta: `${tema12} They _ (start/starts) work very early in the morning and _ (finish/finishes) work late in the afternoon.`,
+        opciones: [
+            'start, finish',
+            'start, finishes',
+            'starts, finish',
+            'starts, finishes',
+        ],
+        respuesta: 'start, finish',
+        img: '',
+    },
+    {
+        pregunta: `${tema12} They _ (live/lives) close to the bank and their house _ (is/are) next to the park, so they _ (drive/drives) to work.`,
+        opciones: [
+            'live, is, drive',
+            'live, is, drives',
+            'live, are, drive',
+            'live, are, drives',
+            'lives, is, drive',
+            'lives, is, drives',
+            'lives, are, drive',
+            'lives, are, drives',
+        ],
+        respuesta: 'live, is, drive',
+        img: '',
+    },
+    {
+        pregunta: `${tema12}`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema12}`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: `${tema12}`,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
+        img: '',
+    },
+    {
+        pregunta: ``,
+        opciones: [
+            '',
+            '',
+            '',
+            '',
+        ],
+        respuesta: '',
         img: '',
     },
 ]
